39ea664e8a74e695d2b2736e77dd1388f0ec6739
LestierAvecilla <lestier.avecilla@my.unitec.edu.mx>
2024-04-12 17:24:05 -0600

    Eliminar archivos y directorios innecesarios

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [animations],
+}
+
